c6144f62152e0933eaa1339c84dc144204a405f2
Aygun <aygun.xuduyeva98@gmail.com>
2023-02-07 09:01:16 -0800

    navbar tamamlandi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef} from 'react'
 //css
 import '../assets/css/Navbar.css'
 //img elave edilmesi
@@ -9,11 +9,24 @@ import { AiOutlineBars } from 'react-icons/ai'
 import Button from './Button'
 
 
+
 function Navbar() {
+ const overLayUnvan = useRef()
+    const operOverLayMenu = (e) => {
+     const kliklediyimunvan = e.target 
+     if(kliklediyimunvan.classList.contains('aibars-icon'))
+            overLayUnvan.current.classList.add('aktiv')
+    }
+const closeOverLayMenu = (e) => {
+    const kliklediyimunvan = e.target
+    if(kliklediyimunvan.classList.contains('gicross-icon')){
+        overLayUnvan.current.classList.remove('aktiv')
+    }
+}
     return (
         <>
-            <div className="overlay">
-                <GiCrossedBones className='gicross-icon' />
+            <div className="overlay" ref={overLayUnvan}>
+                <GiCrossedBones className='gicross-icon' onClick={closeOverLayMenu} />
                 <div className='overlay-nav'>
                     <a href="#">Home</a>
                     <a href="#">About</a>
@@ -26,8 +39,9 @@ function Navbar() {
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#"><img src={logoShekil} alt="suni intellekt" /></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <AiOutlineBars className='aibars-icon' />
+                        <AiOutlineBars className='aibars-icon' onClick={operOverLayMenu} />
                     </button>
+                  
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
