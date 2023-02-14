8541482113b3841678bb5675da699466f703f99f
Aygun <aygun.xuduyeva98@gmail.com>
2023-02-14 03:49:57 -0800

    aos duration elavesi

--- src/components/LearnSection.jsx
+++ src/components/LearnSection.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 import '../assets/css/LearnSection.css'
 import LearnImg from '../assets/images/learnimg.svg'
 import Button from './Button'
+//typed yazi yazma elave edildi
 import Typed from 'react-typed';
 
 const LearnSection = () => {
     return (
         <div className='learnSection row'>
-            <div className='learn-left-side col-12 col-md-6 ' data-aos="fade-up-right"><img className='w-100 h-100' src={LearnImg} alt="" /></div>
-            <div className='learn-rigth-side col-12 col-md-6' data-aos="fade-up-left" >
+            <div className='learn-left-side col-12 col-md-6 ' data-aos="fade-up-right" data-aos-duration="2000"><img className='w-100 h-100' src={LearnImg} alt="" /></div>
+            <div className='learn-rigth-side col-12 col-md-6' data-aos="fade-up-left" data-aos-duration="2000">
                 <h1>Apply AI, Deep Learning and Data Sciece to solve</h1>
                 <Typed className='learnType'
                     strings={['Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.']}
                     typeSpeed={40}
                 />
-                {/* <h5>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</h5> */}
+                
                 <Button  icindekiYazi="Learn More" klassAdi='my-custom-btn'/>
                 
             </div>
